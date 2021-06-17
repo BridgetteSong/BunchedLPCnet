6200389da01ab555ba220cde1a3989a349c51c80
BridgetteSong <957976193@qq.com>
2021-06-17 12:36:20 +0800

    Update hparams.py

--- training_torch/hparams.py
+++ training_torch/hparams.py
@@ -14,17 +14,6 @@ def load_hparam_str(hp_str):
 
 def load_hparam(filename):
     stream = open(filename, "r")
-    # loader = yaml.SafeLoader
-    # loader.add_implicit_resolver(
-    #     u'tag:yaml.org,2002:float',
-    #     re.compile(u'''^(?:
-    #      [-+]?(?:[0-9][0-9_]*)\\.[0-9_]*(?:[eE][-+]?[0-9]+)?
-    #     |[-+]?(?:[0-9][0-9_]*)(?:[eE][-+]?[0-9]+)
-    #     |\\.[0-9_]+(?:[eE][-+][0-9]+)?
-    #     |[-+]?[0-9][0-9_]*(?::[0-5]?[0-9])+\\.[0-9_]*
-    #     |[-+]?\\.(?:inf|Inf|INF)
-    #     |\\.(?:nan|NaN|NAN))$''', re.X),
-    #     list(u'-+0123456789.'))
     docs = yaml.load_all(stream, Loader=yaml.Loader)
     hparam_dict = dict()
     for doc in docs:
